60f85bad9b21f02c47e7dba5caacbbd77cd2cce2
davift <73488542+davift@users.noreply.github.com>
2023-02-11 18:32:22 -0500

    Add files via upload

---
+++ http.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import socket
+import base64
+
+# Specify the IP you want to bind on.
+BIND = '0.0.0.0'
+# To listen on the default port 80 might require root privileges.
+PORT = 80
+
+def listener(BIND, PORT):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((BIND, PORT))
+    server_socket.listen(1)
+    print("Listening on", BIND, "port", PORT)
+
+    try:
+        while True:
+            conn, address = server_socket.accept()
+            print("Connection from:", address[0], "on port:", PORT)
+            data = conn.recv(1024).decode()
+            if data.startswith("GET"):
+                conn.send("HTTP/1.1 401 Unauthorized\r\n".encode())
+                auth = None
+                for header in data.split("\r\n"):
+                    if header.startswith("Authorization: Basic"):
+                        auth = header.split(" ")[-1]
+                        break
+                if auth:
+                    auth = base64.b64decode(auth).decode().split(":")
+                    print("Connection from:", address[0], "on port:", PORT, "username:", auth[0], "password:", auth[1])
+                conn.send("WWW-Authenticate: Basic realm=\"Authentication Required\"\r\n".encode())
+            conn.close()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        conn.close()
+        server_socket.close()
+        exit()
+
+if __name__ == '__main__':
+    listener(BIND, PORT)
+
+exit()
